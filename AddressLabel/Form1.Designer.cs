090756e1c4190d04454bfdf6e44a23fb48757528
abeldoyl <abeldoyle71@gmail.com>
2026-03-09 19:06:26 -0600

    Add group box with First name text box

--- AddressLabel/Form1.Designer.cs
+++ AddressLabel/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AddressLabel
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            MailingGroupBox = new GroupBox();
+            FirstNameLabel = new Label();
+            FirstNameTextBox = new TextBox();
+            MailingGroupBox.SuspendLayout();
+            SuspendLayout();
+            // 
+            // MailingGroupBox
+            // 
+            MailingGroupBox.Controls.Add(FirstNameLabel);
+            MailingGroupBox.Controls.Add(FirstNameTextBox);
+            MailingGroupBox.Location = new Point(12, 12);
+            MailingGroupBox.Name = "MailingGroupBox";
+            MailingGroupBox.Size = new Size(397, 402);
+            MailingGroupBox.TabIndex = 0;
+            MailingGroupBox.TabStop = false;
+            MailingGroupBox.Text = "Mailing Address";
+            // 
+            // FirstNameLabel
+            // 
+            FirstNameLabel.AutoSize = true;
+            FirstNameLabel.Location = new Point(32, 42);
+            FirstNameLabel.Name = "FirstNameLabel";
+            FirstNameLabel.Size = new Size(64, 15);
+            FirstNameLabel.TabIndex = 1;
+            FirstNameLabel.Text = "First Name";
+            // 
+            // FirstNameTextBox
+            // 
+            FirstNameTextBox.Location = new Point(32, 60);
+            FirstNameTextBox.Name = "FirstNameTextBox";
+            FirstNameTextBox.Size = new Size(165, 23);
+            FirstNameTextBox.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(MailingGroupBox);
+            Name = "MainForm";
+            Text = "Address Label Viewer";
+            MailingGroupBox.ResumeLayout(false);
+            MailingGroupBox.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox MailingGroupBox;
+        private Label FirstNameLabel;
+        private TextBox FirstNameTextBox;
     }
 }
